6fb66c21f929663f3808360884b0622f80938028
Paul Rougieux <paul.rougieux@gmail.com>
2021-11-12 18:22:38 +0100

    Update example

--- biotrade/faostat/quality.py
+++ biotrade/faostat/quality.py
@@ -18,19 +18,21 @@ def compare_aggregate_to_constituents(df, prod_aggregate, prod_constituents):
     :param str prod_aggregate: Name of the aggregate product
     :param list prod_constituents: Names of the product constituents
 
+    Return a reshaped data frame in wider format with aggregates and their constituents in columns.
+
     For more information, see the FAOSTAT forest products classification and
-    standards at https://www.fao.org/forestry/statistics/80572/en/ in
-    particular the data structure
+    standards at https://www.fao.org/forestry/statistics/80572/en/
+    in particular the data structure chart
     https://www.fao.org/forestry/49962-0f43c0da7039a611aa884b3c6c642f4ac.pdf
     explains how the different products and aggregates relate to each other.
 
-    Return a reshaped data frame in wider format with aggregates and their constituents in columns.
-
-    For example compare roundwood to the sum of industrial roundwood and fuel wood.
+    For example compare roundwood to the sum of industrial roundwood and fuel
+    wood. In the complete forestry production dataset (including production,
+    import and export values and quantities).
 
     >>> from biotrade.faostat import faostat
     >>> from biotrade.faostat.quality import compare_aggregate_to_constituents
-    >>> fp = faostat.forestry_production
+    >>> fp = faostat.pump.forestry_production
     >>> rwd = compare_aggregate_to_constituents(
     >>>     fp, "roundwood", ["industrial_roundwood", "wood_fuel"]
     >>> )
