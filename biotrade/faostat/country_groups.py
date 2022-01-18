c0dfe845f1a4dfa6b9e07f683de969104d87aced
Paul Rougieux <paul.rougieux@gmail.com>
2022-01-18 09:51:40 +0100

    Replace the continents method by df to be more generic

--- biotrade/faostat/country_groups.py
+++ biotrade/faostat/country_groups.py
@@ -62,6 +62,20 @@ class CountryGroups(object):
         self.config_data_dir = self.parent.config_data_dir
 
     @property
+    def df(self):
+        """Country groupings
+
+        :return: A data frame of table faostat_country_groups.csv
+
+            >>> from biotrade.faostat import faostat
+            >>> faostat.country_groups.df
+
+        """
+        path = self.config_data_dir / "faostat_country_groups.csv"
+        df = pandas.read_csv(path)
+        return df
+
+    @property
     def eu_country_names(self):
         """EU country names in the FAOSTAT data"""
         return EU_COUNTRY_NAMES
@@ -70,8 +84,6 @@ class CountryGroups(object):
     def continents(self):
         """Country groupings by continents and subcontinents
 
-        :return: A data frame of table faostat_country_groups.csv
-
         For example select from the crop trade table soy products and merge
         with the continents and subcontinents data, regarding both reporter
         and partner countries.
@@ -79,14 +91,11 @@ class CountryGroups(object):
         >>> from biotrade.faostat import faostat
         >>> db = faostat.db
         >>> df_soy = db.select(table="crop_trade", product = "soy")
-        >>> df_continents = faostat.country_groups.continents[
-                ['faost_code','continent', 'sub_continent']]
+        >>> df_continents = faostat.country_groups.df
         >>> df_soy_merge = df_soy.merge(df_continents, how='left',
                 left_on = 'reporter_code', right_on = 'faost_code')
         >>> df_soy_merge = df_soy_merge.merge(df_continents, how='left',
                 left_on = 'partner_code', right_on = 'faost_code',
                 suffixes=('_reporter','_partner'))
         """
-        path = self.config_data_dir / "faostat_country_groups.csv"
-        df = pandas.read_csv(path)
-        return df
+        return self.df[["faost_code", "continent", "sub_continent"]]
