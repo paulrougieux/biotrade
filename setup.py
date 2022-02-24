eb4bad591f2e89861aee1ea6afaa3e0be68669fc
Paul Rougieux <paul.rougieux@gmail.com>
2022-02-24 10:51:21 +0100

    Add setup.py to prepare pip installation

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Written by Paul Rougieux and Selene Patani
+MIT Licensed.
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, "README.md")
+with open(readme_path, encoding="utf-8") as handle:
+    readme = handle.read()
+
+# Call setup #
+setup(
+    name="plumbing",
+    version="2.10.7",
+    description="Helps with plumbing-type programing in python.",
+    license="MIT",
+    url="http://github.com/xapple/plumbing/",
+    author="Paul Rougieux",
+    author_email="paul.rougieux@gmail.com",
+    packages=find_namespace_packages(exclude=["notebooks", "scripts"]),
+    install_requires=["pandas", "sqlalchemy", "sqlalchemy_utils"],
+    extras_require={"api": ["fastapi"]},
+    python_requires=">=3.7",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+)
