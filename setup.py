b758270bfc595580f7209b80f0754807c287a820
Paul Rougieux <paul.rougieux@gmail.com>
2022-03-15 10:16:29 +0100

    Increase version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(readme_path, encoding="utf-8") as handle:
 # Call setup #
 setup(
     name="biotrade",
-    version="0.0.1",
+    version="0.0.2",
     description="Agriculture and forestry statistics.",
     license="MIT",
     url="https://gitlab.com/bioeconomy/forobs/biotrade/",
