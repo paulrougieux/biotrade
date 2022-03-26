f4e3bfa2474dcc0410b5bf1753d9fc4f82c41f6e
Paul Rougieux <paul.rougieux@gmail.com>
2022-03-26 15:53:52 +0100

    Correct typo in product mapping and explain that only the lowest level should be stored in that table #21

--- biotrade/common/products.py
+++ biotrade/common/products.py
@@ -16,19 +16,29 @@ Usage:
 Source  http://datalab.review.fao.org/datalab/caliper/web/classification-page/41
 FAOSTAT Commodity List (FCL) "Download mappings" with 2 csv tables:
 
-http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/FCL_HS_mappings_2020-01-07.csv
-which contains FCL codes, labels and the correspondence with Comtrade HS codes.
+- http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/FCL_HS_mappings_2020-01-07.csv
+  which contains FCL codes, labels and the correspondence with Comtrade HS codes.
 
-http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/HS_FCL_mappings_2020-01-07.csv
-which contains Comtrade HS codes, Comtrade HS labels and the correspondence with FCL.
+- http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/HS_FCL_mappings_2020-01-07.csv
+  which contains Comtrade HS codes, Comtrade HS labels and the correspondence with FCL.
 
 comtrade_faostat_mapping was made from the second table, HS_FCL mapping.
+
+
+Only the lowest level of FAOSTAT product codes should be entered in this table.
+Groups such as the faostat product sawnwood which is an aggregate of sawnwood
+non coniferous and sawnwood coniferous should be handled at another level and
+only the lowest level product codes of sawnwood non coniferous and sawnwood
+coniferous should be present in the mapping table.
+
 """
 # Third party modules
 import pandas
 from biotrade import module_dir
 
 config_data_dir = module_dir / "config_data"
+# Note the variable name is lower case as suggested in PEP 8
+# https://peps.python.org/pep-0008/#function-and-variable-names
 comtrade_faostat_mapping = pandas.read_csv(
     config_data_dir / "comtrade_faostat_product_mapping.csv"
 )
