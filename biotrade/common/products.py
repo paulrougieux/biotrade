a211a82c52dd422219f5727640a6b0f216d7ff42
Paul Rougieux <paul.rougieux@gmail.com>
2022-03-26 07:25:06 +0100

    Document the origin of the data #21

--- biotrade/common/products.py
+++ biotrade/common/products.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Written by Paul Rougieux.
+Written by Paul Rougieux and Selene Patani.
 
 JRC biomass Project.
 Unit D1 Bioeconomy.
@@ -13,6 +13,16 @@ Usage:
 
     >>> from biotrade.common.products import comtrade_faostat_mapping
 
+Source  http://datalab.review.fao.org/datalab/caliper/web/classification-page/41
+FAOSTAT Commodity List (FCL) "Download mappings" with 2 csv tables:
+
+http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/FCL_HS_mappings_2020-01-07.csv
+which contains FCL codes, labels and the correspondence with Comtrade HS codes.
+
+http://datalab.review.fao.org/datalab/caliper/web/sites/default/files/2020-01/HS_FCL_mappings_2020-01-07.csv
+which contains Comtrade HS codes, Comtrade HS labels and the correspondence with FCL.
+
+comtrade_faostat_mapping was made from the second table, HS_FCL mapping.
 """
 # Third party modules
 import pandas
