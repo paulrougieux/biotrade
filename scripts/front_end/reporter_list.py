cfb806a8ea96c2b4f139775ad0a79cdca831e7c1
selenepatani91 <selene.patani@ext.ec.europa.eu>
2022-05-17 13:05:46 +0200

    #65 modified reporter list

--- scripts/front_end/reporter_list.py
+++ scripts/front_end/reporter_list.py
@@ -9,15 +9,20 @@ import pandas as pd
 from biotrade import data_dir
 from biotrade.faostat import faostat
 
-# Name of product file to retrieve
+# Name of reporter file to retrieve
 reporter_file = faostat.config_data_dir / "faostat_country_groups.csv"
 # Retrieve dataset
 reporter = pd.read_csv(reporter_file)
 # Columns to be retained and to change name
 column_dict = {"iso3_code": "reporter_code", "fao_table_name": "reporter_name"}
+# Drop rows where iso3 codes are nan
 reporter = reporter[column_dict.keys()].rename(columns=column_dict).dropna()
+# Add OTH - Others as reporter code and reporter
+reporter = pd.concat(
+    [reporter, pd.DataFrame([["OTH", "Others"]], columns=reporter.columns)],
+    ignore_index=True,
+)
 # Save csv files
 folder_path = data_dir / "front_end"
-if not folder_path.exists():
-    folder_path.mkdir()
+folder_path.mkdir(exist_ok=True)
 reporter.to_csv(folder_path / "reporter_list.csv", index=False)
