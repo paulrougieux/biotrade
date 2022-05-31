91578a03c1080f367d53816de1696838a3880704
Paul Rougieux <paul.rougieux@gmail.com>
2022-05-31 18:43:24 +0200

    Double the wait time at each attempt fixes #73

--- biotrade/comtrade/pump.py
+++ biotrade/comtrade/pump.py
@@ -169,10 +169,11 @@ class Pump:
         # Variable for checking successfull zip dowload
         download_successful = False
         # Number of attempts to download zip file
-        nr_try = 1
+        sleep_time = 5
         # Send the request: if successful put the zip file into the temporary
-        # folder else if more than 1000 attempts loop stop
-        while not download_successful and nr_try <= 1000:
+        # folder else retry and double the wait time at each try
+        # If the wait raises to more than 20 minutes of wait time stop.
+        while not download_successful and sleep_time < 1200:
             try:
                 with urllib.request.urlopen(req) as response, open(
                     temp_file, "wb"
@@ -185,14 +186,10 @@ class Pump:
                 # Error 404 means that no data are present, stop the while loop
                 if error.code == 404:
                     download_successful = True
-                else:
-                    nr_try += 1
-                    time.sleep(5)
                 response_code = error.code
             except urllib.error.URLError as error:
-                nr_try += 1
-                time.sleep(5)
                 response_code = error.reason
+            sleep_time *= 2
             self.logger.info(f"HTTP response code: {response_code}")
         return temp_dir, response_code
 
@@ -278,7 +275,9 @@ class Pump:
             # Delete already existing data
             if check_data_presence:
                 self.db.delete_data(
-                    table_name, api_period, api_period,
+                    table_name,
+                    api_period,
+                    api_period,
                 )
             # Append data to db
             self.db.append(df, table_name)
@@ -287,7 +286,12 @@ class Pump:
         return records_downloaded_csv
 
     def transfer_bulk_csv(
-        self, table_name, start_year, end_year, frequency, check_data_presence,
+        self,
+        table_name,
+        start_year,
+        end_year,
+        frequency,
+        check_data_presence,
     ):
         """
         Pump method to transfer bulk csv file of Comtrade API requests to
@@ -377,7 +381,10 @@ class Pump:
                 # Construct the period to pass to transfer_csv_chunk method
                 api_period = int(str(period) + month)
                 # Store zip data into the temporary directory
-                temp_dir, response_code = self.download_bulk_csv(api_period, frequency,)
+                temp_dir, response_code = self.download_bulk_csv(
+                    api_period,
+                    frequency,
+                )
                 # If data are downloaded (response = 200) copy the csv of
                 # the zip file into a pandas data frame
                 if response_code == 200:
@@ -425,9 +432,7 @@ class Pump:
             )
         return period_list_failed
 
-    def update_db(
-        self, table_name, frequency,
-    ):
+    def update_db(self, table_name, frequency, start_year=None):
         """
         Pump method to update db. If data from 2016 are already present,
         it updates data of the last and current year, otherwise it uploads
@@ -435,33 +440,42 @@ class Pump:
 
         :param (string) table_name, name of Comtrade db table
         :param (string) frequency, "M" for monthly data or "A" for annual
+        :param (int) start_year, year to start the download from, defaults to
+            2016 if not specified
 
-        return an error if some periods are not uploaded do db
+        Return an error if some periods are not uploaded to the database.
 
         Example of updating db table "monthly" with monthly frequency data:
 
-        >>> from biotrade.comtrade import comtrade
-        >>> comtrade.pump.update_db(table_name = "monthly", frequency = "M")
+            >>> from biotrade.comtrade import comtrade
+            >>> comtrade.pump.update_db(table_name = "monthly", frequency = "M")
+
         """
-        # Adjust frequency parameter in case it is wrong provided
+        if start_year is None:
+            start_year = 2016
+        # Adjust frequency parameter in case it is wrongly provided
         if table_name in ("yearly", "yearly_hs2"):
             frequency = "A"
         elif table_name == "monthly":
             frequency = "M"
         current_year = datetime.datetime.now(pytz.timezone("Europe/Rome")).date().year
-        # Check if data from 2016 are present into db
-        data_check = self.db.check_data_presence(
-            table_name, 2016, current_year, frequency,
+        # Check if data from the start year are present into the database
+        data_present = self.db.check_data_presence(
+            table_name,
+            start_year,
+            current_year,
+            frequency,
         )
-        # If data are already inside db, update from the last year
-        if data_check:
+        # If data are already inside DB, update from the year before last only
+        if data_present:
             start_year = current_year - 1
-        # Update from 2016
-        else:
-            start_year = 2016
         # Transfer from api bulk requests to db
         period_list_failed = self.transfer_bulk_csv(
-            table_name, start_year, current_year, frequency, data_check,
+            table_name,
+            start_year,
+            current_year,
+            frequency,
+            data_present,
         )
         # If some periods failed to be uploaded, raise an error
         if len(period_list_failed):
