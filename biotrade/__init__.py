e866603092fe60c2081754fbbcc217c86dd31c7a
Paul Rougieux <paul.rougieux@gmail.com>
2022-11-14 09:52:27 +0100

    Bump version: 0.0.12 → 0.0.13

--- biotrade/__init__.py
+++ biotrade/__init__.py
@@ -22,7 +22,7 @@ Usage:
 from pathlib import Path
 import os
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 module_dir = Path(__file__).resolve().parent
 
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(readme_path, encoding="utf-8") as handle:
 # Call setup #
 setup(
     name="biotrade",
-    version="0.0.12",
+    version="0.0.13",
     description="Agriculture and forestry statistics.",
     license="MIT",
     url="https://gitlab.com/bioeconomy/forobs/biotrade/",
