d0ad7e20e4d51447e75bd0109534381e57c222f3
Paul Rougieux <paul.rougieux@gmail.com>
2023-05-10 19:31:07 +0200

    Change status from alpha to beta

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
