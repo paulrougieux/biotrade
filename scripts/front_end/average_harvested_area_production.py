d94178875882eab324710eb789b4861051b879e2
selenepatani91 <selene.patani@ext.ec.europa.eu>
2023-06-19 15:47:29 +0200

    updated average_harvested_area_production.py

--- scripts/front_end/average_harvested_area_production.py
+++ scripts/front_end/average_harvested_area_production.py
@@ -16,7 +16,7 @@ def main():
     from biotrade.faostat import faostat
     from scripts.front_end.functions import COLUMN_PERC_SUFFIX
     from scripts.front_end.functions import (
-        consistency_check_china_data,
+        aggregated_data,
         main_product_list,
         average_results,
         reporter_iso_codes,
@@ -25,7 +25,9 @@ def main():
     )
 
     # Obtain the main product codes
-    main_product_list = main_product_list(["crop_production", "forestry_production"])
+    main_product_list = main_product_list(
+        ["crop_production", "forestry_production"]
+    )
     # Query db to obtain data
     crop_df = faostat.db.select(
         table="crop_production",
@@ -42,10 +44,14 @@ def main():
     df = pd.concat([crop_df, wood_df], ignore_index=True)
     # Select only reporter with code lower than 1000
     df = df[df["reporter_code"] < 1000].reset_index(drop=True)
-    # China Mainland + Taiwan data
-    df_china = consistency_check_china_data(df)
-    # Add China data to df (exclude Taiwan data)
-    df = pd.concat([df[~(df["reporter_code"] == 214)], df_china], ignore_index=True)
+    # Aggregate french territories values to France and add them to the dataframe
+    code_list = [68, 69, 87, 135, 182, 270, 281]
+    agg_country_code = 68
+    agg_country_name = faostat.country_groups.df
+    agg_country_name = agg_country_name[
+        agg_country_name.faost_code == agg_country_code
+    ].fao_table_name.values[0]
+    df = aggregated_data(df, code_list, agg_country_code, agg_country_name)
     # Substitute faostat codes with iso3 codes
     df = reporter_iso_codes(df)
     # Define the columns and codes for the average calculations
@@ -82,13 +88,19 @@ def main():
     column_list = df_final.columns.tolist()
     column_list.remove(drop_column)
     # Define dropna columns
-    dropna_col = [col for col in df_final.columns if col.endswith(COLUMN_PERC_SUFFIX)]
+    dropna_col = [
+        col for col in df_final.columns if col.endswith(COLUMN_PERC_SUFFIX)
+    ]
     df_final = replace_zero_with_nan_values(df_final, dropna_col)
     df_final = df_final.dropna(subset=dropna_col, how="all")
     # Save csv files to env variable path or into biotrade data folder
-    harvested_area = df_final[df_final["element"] == "area_harvested"][column_list]
+    harvested_area = df_final[df_final["element"] == "area_harvested"][
+        column_list
+    ]
     save_file(harvested_area, "harvested_area_average.csv")
-    production = df_final[df_final["element"].isin(["production", "stocks"])][column_list]
+    production = df_final[df_final["element"].isin(["production", "stocks"])][
+        column_list
+    ]
     save_file(production, "production_average.csv")
 
 
