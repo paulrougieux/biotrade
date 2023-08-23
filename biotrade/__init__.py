e4d7c06f0385362b4fd340ab2080eb3b732276e9
selenepatani91 <selene.patani@ext.ec.europa.eu>
2023-08-23 14:38:48 +0200

    Bump version: 0.2.6 → 0.2.7

--- biotrade/__init__.py
+++ biotrade/__init__.py
@@ -77,7 +77,7 @@ Some paths are defined at the top level:
 from pathlib import Path
 import os
 
-__version__ = "0.2.6"
+__version__ = "0.2.7"
 
 module_dir = Path(__file__).resolve().parent
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(readme_path, encoding="utf-8") as handle:
 # Call setup #
 setup(
     name="biotrade",
-    version="0.2.6",
+    version="0.2.7",
     description="Agriculture and forestry statistics.",
     license="MIT",
     url="https://gitlab.com/bioeconomy/forobs/biotrade/",
