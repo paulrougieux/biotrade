35bd3e1b22b69c7c2e6527427eed901667e56bc0
selenepatani91 <selene.patani@ext.ec.europa.eu>
2024-10-08 16:10:56 +0200

    Bump version: 0.3.16 → 0.3.17

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open(readme_path, encoding="utf-8") as handle:
 # Call setup #
 setup(
     name="biotrade",
-    version="0.3.16",
+    version="0.3.17",
     description="Agriculture and forestry statistics.",
     license="MIT",
     url="https://gitlab.com/bioeconomy/forobs/biotrade/",
