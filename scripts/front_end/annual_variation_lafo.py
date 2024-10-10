c63210568f24b55644944057cec229059a6f0803
selenepatani91 <selene.patani@ext.ec.europa.eu>
2024-10-10 17:49:56 +0200

    modified annual_variation_lafo for eu aggregation

--- scripts/front_end/annual_variation_lafo.py
+++ scripts/front_end/annual_variation_lafo.py
@@ -11,6 +11,7 @@ Script made to export data for the EUFO web platform, related to annual lafo val
 
 def main():
     from scripts.front_end.functions import (
+        remove_intra_eu_values,
         country_names,
         aggregated_data,
         reporter_iso_codes,
@@ -41,11 +42,13 @@ def main():
     lafo_data = reporter_iso_codes(lafo_data, col="iso2_code")
     # Aggregate by reporters
     group_by_cols = ["reporter_code", "product_code", "primary_code", "period", "unit"]
-    lafo_data = lafo_data.groupby(group_by_cols)["value"].agg("sum").reset_index()
+    lafo_data_reporter = (
+        lafo_data.groupby(group_by_cols)["value"].agg("sum").reset_index()
+    )
     # Remove nan data
     dropna_col = ["value"]
-    lafo_data = replace_zero_with_nan_values(lafo_data, dropna_col)
-    lafo_data = lafo_data.dropna(subset=dropna_col)
+    lafo_data_reporter = replace_zero_with_nan_values(lafo_data_reporter, dropna_col)
+    lafo_data_reporter = lafo_data_reporter.dropna(subset=dropna_col)
     # Columns to be finally retained
     column_list = [
         "reporter_code",
@@ -55,30 +58,31 @@ def main():
         "unit",
     ]
     # Save data
-    save_file(lafo_data[column_list], "lafo_reporter_annual_variation.csv")
-    # Aggregate at level of primary code
-    group_by_cols.remove("product_code")
-    eu_row_data = lafo_data.groupby(group_by_cols)["value"].agg("sum").reset_index()
+    save_file(lafo_data_reporter[column_list], "lafo_reporter_annual_variation.csv")
     # Add reporter and partner names to aggregate at EU, ROW level
-    eu_row_data = country_names(eu_row_data, "iso3_code")
-    eu_row_data["partner"] = None
+    eu_row_data = country_names(lafo_data, "iso3_code")
+    # Remove EU internal trades
+    eu_row_data = remove_intra_eu_values(eu_row_data)
     # Aggregate to EU and ROW for reporters
     eu_row_data = agg_trade_eu_row(
         eu_row_data,
         grouping_side="reporter",
     )
+    # Substitute with name and codes of the aggregations for the web platform
+    selector = eu_row_data["reporter"] == "eu"
+    eu_row_data.loc[selector, "reporter_code"] = "EU27"
+    eu_row_data.loc[~selector, "reporter_code"] = "ROW"
+    # Aggregate at level of primary code
+    group_by_cols.remove("product_code")
+    eu_row_data = eu_row_data.groupby(group_by_cols)["value"].agg("sum").reset_index()
     # Calculate the percentage share of EU and ROW
     group_by_cols.remove("reporter_code")
     eu_row_data["sum_value"] = eu_row_data.groupby(group_by_cols)["value"].transform(
         "sum"
     )
     eu_row_data["value_share"] = eu_row_data["value"] / eu_row_data["sum_value"] * 100
-    # Substitute with name and codes of the aggregations for the web platform
-    selector = eu_row_data["reporter"] == "eu"
-    eu_row_data.loc[selector, "reporter_code"] = "EU27"
-    eu_row_data.loc[~selector, "reporter_code"] = "ROW"
     # Consider selected columns of import quantities and values and save the files (drop nan)
-    dropna_col = ["value"]
+    dropna_col = ["value", "value_share"]
     eu_row_data = replace_zero_with_nan_values(eu_row_data, dropna_col)
     eu_row_data = eu_row_data.dropna(subset=dropna_col)
     # Columns to be finally retained
